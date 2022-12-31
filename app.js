80e5a8e8370bad8c2ab8f889521d0d89d1ecd70a
UjjawalChaudhary <89735684+UjjawalChaudhary@users.noreply.github.com>
2022-12-31 10:47:15 +0530

    Urls and BodyParser

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
     next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<h1>Hello from node js</h1>');
-    next();
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from node js</h1>');
+
+})
 
 app.listen(3000);
