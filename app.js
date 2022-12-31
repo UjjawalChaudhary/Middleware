4ba8e2374a77698547756ca363242239e826a109
UjjawalChaudhary <89735684+UjjawalChaudhary@users.noreply.github.com>
2022-12-31 13:34:52 +0530

    CleanUp

--- app.js
+++ app.js
@@ -3,25 +3,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next(); // Allows the request to continue to the next middleware in line
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+
+
+// app.use('/', (req, res, next) => {
+//     console.log('This always runs!');
+//     next(); // Allows the request to continue to the next middleware in line
+// });
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/',(req,res,next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from node js</h1>');
-
-})
 
 app.listen(3000);
